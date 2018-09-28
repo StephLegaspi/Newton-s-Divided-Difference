90fff7d30788226458f79009a8036b6c972c95ac
StephLegaspi <sylegaspi@up.edu.ph>
2018-09-28 22:02:28 +0800

    NDD done with polynomial

--- legaspi_ex6.R
+++ legaspi_ex6.R
@@ -1,8 +1,6 @@
 x = c(8, 9, 11, 12)
 y = c(0.9031, 0.9542, 1.0414, 1.0792)
 
-o = 3
-
 RowColNames <- function(size){
   rcnames = c()
   for (i in 1:size) {
@@ -33,11 +31,42 @@ GetOrders <- function(x, y, order, m){
   return(m)
 }
 
+GetMultipliers <- function(x, i, term){
+  
+  for(j in 1:i){
+    term = paste(term, "(x - ", sep = " * ")
+    term = paste(term, x[j], sep = "")
+    term = paste(term, ")", sep = "")
+  }
+  
+  return(term)
+}
+
+SetUpPolynomial <- function(x, y, m, order){
+  term = y[1]
+  row = 1
+  for(i in 1:order){
+    term = paste(term, "+", sep = " ")
+    term = paste(term, m[1, i], sep = " ")
+    
+    term = GetMultipliers(x, i, term)
+  }
+  return(term)
+}
+
 NDD <- function(x, y, order){
   m = matrix(data=NA, nrow=order, ncol=order, dimnames = list(RowColNames(order), RowColNames(order)))
   m = FirstOrder(x, y, order, m)
-  m = GetOrders(x, y, order, m)
-  print(m)
+  if(order != 1){
+    m = GetOrders(x, y, order, m)
+  }
+  polynomial = SetUpPolynomial(x, y, m, order)
+  args <- "x"
+  result = list(func = eval(parse(text = paste('f <- function(', args, ') { return(' , polynomial , ')}', sep=''))))
+  return(result)
+  
 }
 
-NDD(x, y, o)
+result = NDD(x, y, 3)
+print(result$func(10))
+
